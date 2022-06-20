e66e8c0e6248a4368b6aeb2f65c16821d773c52d
Lukkiness <90136472+Lukkiness@users.noreply.github.com>
2022-06-20 17:02:37 +0900

    dd

--- app/src/main/java/com/example/smarthomeapp/model/MemberListData.java
+++
@@ -1,9 +0,0 @@
-package com.example.smarthomeapp.model;
-
-import com.google.gson.annotations.SerializedName;
-
-import java.util.ArrayList;
-
-public class MemberListData extends BaseResponse {
-    @SerializedName("data") public ArrayList<Member> memberList;
-}
