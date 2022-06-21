5a256cd5e7b32058963f530f70bbd0271d8937df
Sulfur-Parrot <sulfurparrot@gmail.com>
2022-06-21 15:43:20 +0900

    home_04
    홈 화면 최근 경고 시각 표시 완료

--- app/src/main/java/com/example/smarthomeapp/BackgroundService.java
+++ app/src/main/java/com/example/smarthomeapp/BackgroundService.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
@@ -61,8 +62,16 @@ public class BackgroundService extends Service {
             @Override
             public void run() {
                 //여기에 지속적으로 돌아가야할 작업을 넣는다
+                String[] times = {"null", "null", "null"};
+                SharedPreferences sharedPreferences = getSharedPreferences("alertTime", Service.MODE_PRIVATE);
+                SharedPreferences.Editor alertEdit = sharedPreferences.edit();
+
+                times[0] = sharedPreferences.getString("room", null);
+                times[1] = sharedPreferences.getString("toilet", null);
+                times[2] = sharedPreferences.getString("kitchen", null);
+
+
                 while(true) {
-                    String[] times = {"null", "null", "null"};
                     r = "";
                     k = "";
                     t = "";
@@ -90,26 +99,32 @@ public class BackgroundService extends Service {
                                 String ktime = kitchenObject.getString("time");
 
                                 if (room.equals("1") || kitchen.equals("1") || toilet.equals("1")) {
-                                    Log.d("서비스 비상시", "경고!");
+                                    Log.d("서비스", "비상시");
                                     //System.out.println("!!경고!! " + " 방: " + room + " 주방: " + kitchen + " 화장실: " + toilet);
                                     if(room.equals("1")) {
                                         r = "방";
-                                        times[0] = rtime;
+                                        alertEdit.putString("room", rtime);
+                                        alertEdit.apply();
+                                        times[0] = sharedPreferences.getString("room", null);
                                     }
 
                                     if(toilet.equals("1")) {
                                         t = "화장실";
-                                        times[1] = ttime;
+                                        alertEdit.putString("toilet", ttime);
+                                        alertEdit.apply();
+                                        times[1] = sharedPreferences.getString("toilet", null);
                                     }
 
                                     if(kitchen.equals("1")) {
                                         k = "주방";
-                                        times[2] = ktime;
+                                        alertEdit.putString("kitchen", ktime);
+                                        alertEdit.apply();
+                                        times[2] = sharedPreferences.getString("kitchen", null);
                                     }
 
                                     sendNotification(r, k, t);
                                 } else {
-                                    Log.d("서비스 평상시", "문제 없음");
+                                    Log.d("서비스", "평상시");
                                 }
 
                                 Intent alertintent = new Intent(getApplicationContext(), MainActivity.class);
