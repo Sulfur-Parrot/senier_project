16a7f5185f19b8d55ac276e62126f3ca5b5d9867
Sulfur-Parrot <sulfurparrot@gmail.com>
2022-07-20 12:24:42 +0900

    AlertListRequest
    php로 db에서 경고 떴던 데이터 가져오기

---
+++ app/src/main/java/com/example/smarthomeapp/AlertListRequest.java
@@ -0,0 +1,29 @@
+package com.example.smarthomeapp;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AlertListRequest extends StringRequest {
+
+    //서버 url 설정(php파일 연동)
+    final static private String URL = "http://220.149.148.40/php_appalert.php";
+    private Map<String, String> map;
+
+
+    public AlertListRequest(Response.Listener<String> listener) {
+        super(Request.Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
